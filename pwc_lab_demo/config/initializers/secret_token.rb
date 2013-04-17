80f68106eed81958f19015d5ddb374a934f5d6c3
Jeremy <bao1018@hotmail.com>
2013-04-17 08:55:43 -0700

    add rails support

---
+++ pwc_lab_demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PwcLabDemo::Application.config.secret_token = 'bd788e3876bf9bcd8d0621221a3113135296aba631d497e5cb17cc19400ae6c79c9d91446b27ca08728cf0d6eb1cd92ce5fb41de98b1b47dc25e13d6bd51917d'
