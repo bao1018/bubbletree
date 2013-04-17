0105f2188654be81ac5ae81b32a24ef4c5f9fa18
Jeremy <bao1018@hotmail.com>
2013-04-17 09:17:33 -0700

    make the demo work on the rails app

---
+++ pwc_lab_demo/app/assets/javascripts/cofog.js
@@ -0,0 +1,105 @@
+/*
+ * BubbleTree Style for COFOG taxonomy
+ *
+ */
+ 
+if (!BubbleTree) window.alert('You must include the BubbleTree before including the styles');
+
+BubbleTree.Styles = BubbleTree.Styles || {};
+
+BubbleTree.Styles.Cofog = {
+	'01': { icon: '/assets/government-uk.svg', color: '#C75746' },
+	'01.1': { icon: '/assets/legislative.svg' },
+	'01.1.1': { icon: '/assets/legislative.svg' },
+	'01.1.2': { icon: '/assets/pig.svg' },
+	'01.1.3': { icon: '/assets/worldmap.svg' },
+	'01.2': { icon: '/assets/aid.svg' },
+	'01.2.1': { icon: '/assets/aid-developing-countries.svg' },
+	'01.2.2': { icon: '/assets/economic-aid.svg' },
+	'01.3': { icon: '/assets/misc-services.svg' },
+	'01.3.1': { icon: '/assets/human-resources.svg' },
+	'01.3.2': { icon: '/assets/planning.svg' },
+	'01.3.3': { icon: '/assets/research.svg' },
+	'01.7': { icon: '/assets/public-debt.svg' },
+	
+	'02': { icon: '/assets/defence.svg', color: '#0AB971' },
+	'02.1': { icon: '/assets/military.svg' },
+	'02.2': { icon: '/assets/civil-defence.svg' },
+	'02.3': { icon: '/assets/foreign-military-aid.svg' },
+	'02.4': { icon: '/assets/defence-research.svg' },
+	'02.5': { icon: '/assets/defence-admin.svg' },
+	
+	'03': { icon: '/assets/order-safety.svg', color: '#EC2406' }, 
+	'03.1': { icon: '/assets/police.svg' },
+	'03.2': { icon: '/assets/fire-brigade.svg' },
+	'03.3': { icon: '/assets/courts.svg' },
+	'03.4': { icon: '/assets/prisons.svg' },
+	'03.5': { icon: '/assets/rd-order-safety.svg' },
+	'03.6': { icon: '/assets/admin-order-safety.svg' },
+	
+	'04': { icon: '/assets/social-systems.svg', color: '#790586' }, 
+	'04.1': { icon: '/assets/social-systems.svg' },
+	'04.1.1': { icon: '/assets/social-systems.svg' },
+	'04.1.2': { icon: '/assets/labour.svg' },					
+	'04.2': { icon: '/assets/farms.svg' },
+	'04.2.1': { icon: '/assets/farms.svg' },
+	'04.2.2': { icon: '/assets/forest.svg' },
+	'04.2.3': { icon: '/assets/fishing.svg' },				
+	'04.3': { icon: '/assets/energy.svg' },
+	'04.3.1': { icon: '/assets/coal.svg' },
+	'04.3.2': { icon: '/assets/petrol.svg' },
+	'04.3.3': { icon: '/assets/nuclear.svg' },
+	'04.3.4': { icon: '/assets/fuel.svg' },
+	'04.3.5': { icon: '/assets/electricity.svg' },
+	'04.3.6': { icon: '/assets/wind.svg' },
+	'04.4': { icon: '/assets/manufactoring-construction.svg' },
+	'04.5': { icon: '/assets/transport.svg' },
+	'04.5.1': { icon: '/assets/car.svg' },
+	'04.5.2': { icon: '/assets/anchor.svg' },
+	'04.5.3': { icon: '/assets/railways.svg' },
+	'04.5.4': { icon: '/assets/airplane.svg' },
+	'04.6': { icon: '/assets/police.svg' },
+	'04.7': { icon: '/assets/police.svg' },
+	'04.8': { icon: '/assets/rd-eco.svg' },
+	'04.9': { icon: '/assets/police.svg' },
+	
+	'05': { icon: '/assets/environment.svg', color: '#2A3A03' },	
+	'05.1': { icon: '/assets/waste.svg' },
+	'05.2': { icon: '/assets/toilet.svg' },
+	'05.3': { icon: '/assets/pollution.svg' },
+	'05.4': { icon: '/assets/tree.svg' },
+	'05.6': { icon: '/assets/environment-admin.svg' },
+	
+	'06': { icon: '/assets/our-streets.svg', color: '#D33673' },		
+	'06.1': { icon: '/assets/housing.svg' },		
+	'06.2': { icon: '/assets/community.svg' },
+	'06.3': { icon: '/assets/water.svg' },
+	'06.4': { icon: '/assets/street-lights.svg' },
+	
+	'07': { icon: '/assets/health.svg', color: '#4E6D00' },											'07.1': { icon: '/assets/medical-supplies.svg' },				
+	'07.1.1': { icon: '/assets/medical-supplies.svg' },				
+	'07.1.2': { icon: '/assets/other-medical-products.svg' },				
+	'07.1.3': { icon: '/assets/wheelchair.svg' },		
+	'07.2': { icon: '/assets/health.svg' },
+	'07.2.1': { icon: '/assets/health.svg' },
+	'07.2.2': { icon: '/assets/microscope.svg' },
+	'07.2.2': { icon: '/assets/dental.svg' },
+	'07.3': { icon: '/assets/hospital.svg' },
+	'07.3.1': { icon: '/assets/hospital.svg' },
+	'07.3.2': { icon: '/assets/hospital-specialized.svg' },
+	'07.3.2': { icon: '/assets/dental.svg' },
+	
+	'08': { icon: '/assets/culture.svg', color: '#938626' },					
+	'08.2': { icon: '/assets/culture.svg' },					
+	'08.1': { icon: '/assets/sports.svg' },	
+	'08.3': { icon: '/assets/media.svg' },	
+	'09': { icon: '/assets/education.svg' },				
+	
+	'10': { icon: '/assets/helping-others.svg', color: '#935B3B' },
+	'10.1': { icon: '/assets/helping-others.svg' },
+	'10.1.2': { icon: '/assets/helping-others.svg' },
+	'10.2': { icon: '/assets/old-age.svg' },
+	'10.4': { icon: '/assets/family.svg' },
+	'10.7': { icon: '/assets/family2.svg' }
+
+};
